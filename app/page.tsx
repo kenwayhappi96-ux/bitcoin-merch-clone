3d5af54fe3c02b20dfe3dadf8a70cf6b042850ab
happi <kenwayhappi@gmail.com>
2026-01-16 15:21:37 +0100

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default async function Home() {
 
               <div className="flex items-center gap-3">
                 <Image 
-                  src="/trustpilot-logo.png" 
+                  src="/ref/trustpilot-logo.png" 
                   alt="Trustpilot" 
                   width={120} 
                   height={40} 
